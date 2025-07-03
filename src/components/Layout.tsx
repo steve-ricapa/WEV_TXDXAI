d793d9bee04e16728c85ffe5d36a946cb9d4a784
Steve Ricapa <147581880+steve-ricapa@users.noreply.github.com>
2025-07-03 18:28:41 -0500

    15 navbar orientation (#17)
    * Navbar Horizontal
    
    * Navbar Horizontal 2
    
    ---------
    
    Co-authored-by: steve-ricapa <steve.ricapa@gmail.com>

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,8 +4,13 @@ import { Outlet } from 'react-router-dom';
 
 const Layout: React.FC = () => {
   return (
-    <div className="flex h-screen">
-      <Navbar />
+    <div className="flex flex-col h-screen">
+      {/* Navbar arriba */}
+      <header className="bg-gray-800 text-white">
+        <Navbar />
+      </header>
+
+      {/* Contenido principal debajo */}
       <main className="flex-1 bg-gray-100 p-6 overflow-auto">
         <Outlet />
       </main>
